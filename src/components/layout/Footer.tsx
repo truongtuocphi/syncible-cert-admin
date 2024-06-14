e5ecd3fa870614e43c8aff6eb6d969a555398c48
truongpd <truongpd@alphatrue.com>
2024-06-14 20:12:09 +0800

    [UPDATE] Change EN -> VI lg

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,9 +5,9 @@ import { AlphaTrueLogoBlueIcon, AlphaTrueLogoWhiteIcon } from '@/assets/icons';
 const Footer = ({ white = false }: { white?: boolean }) => {
   return (
     <div className="px-4 text-center md:px-8 xl:px-12">
-      <div>Copyright © {new Date().getFullYear()} ABAII. All rights reserved.</div>
+      <div>Bản quyền © {new Date().getFullYear()} ABAII. Đã đăng ký bản quyền.</div>
       <div className="mt-2 flex items-center justify-center gap-1">
-        Powered by{' '}
+        Cung cấp bởi{' '}
         <Link href="https://alphatrue.com" className={white ? 'h-6 w-24' : 'h-8 w-32'}>
           {white ? <AlphaTrueLogoWhiteIcon className="ml-0.5" /> : <AlphaTrueLogoBlueIcon />}
         </Link>

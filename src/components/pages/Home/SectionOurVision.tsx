55de4cf100889bc57a27b3fef59c424e311bb7a0
Truong Tuoc Phi <truongphi754@gmail.com>
2024-08-02 20:15:27 +0700

    feat: update SectionWhatBeLieve and SectionOurVison

--- src/components/pages/Home/SectionOurVision.tsx
+++ src/components/pages/Home/SectionOurVision.tsx
@@ -1,12 +1,11 @@
 export default function SectionOurVision() {
   return (
-    <div className="mt-20 md:mt-40 lg:mt-80 px-6 md:px-14 lg:px-24">
+    <div className="mt-20 px-6 md:mt-40 md:px-14 lg:mt-80 lg:px-24">
       <h1 className="text-4xl font-bold">Our vision</h1>
-      <div className="md:w-4/6 lg:w-1/2 mt-4">
-        Syncible aims to create a comprehensive shift in the recognition of
-        academic achievements, transcending geographical boundaries and
-        administrative procedures, allowing students to transparently and
-        reliably demonstrate their skills to employers and educational
+      <div className="mt-4 md:w-4/6 lg:w-1/2">
+        Syncible aims to create a comprehensive shift in the recognition of academic achievements,
+        transcending geographical boundaries and administrative procedures, allowing individuals to
+        transparently and reliably demonstrate their skills to employers and educational
         institutions.
       </div>
     </div>
--- src/components/pages/Home/SectionWhatWeBelieve.tsx
+++ src/components/pages/Home/SectionWhatWeBelieve.tsx
@@ -1,17 +1,16 @@
 export default function SectionWhatWeBelieve() {
   return (
-    <div className="mt-20 md:mt-40 lg:mt-80 px-6 md:px-14 lg:px-24">
+    <div className="mt-20 px-6 md:mt-40 md:px-14 lg:mt-80 lg:px-24">
       <h1 className="flex justify-end text-4xl font-bold">What we believe</h1>
       <div className="flex justify-end">
-        <div className="w-full md:w-4/6 lg:w-1/2 mt-4 text-right">
-          By tokenizing diplomas, certificates, and educational accomplishments,
-          we help students build an authentic, verifiable digital portfolio that
-          provides a comprehensive view of their skills and knowledge. Syncible
-          reduces the time to issue certificates, printing costs, and
-          educational fees, while eliminating the need to retrieve physical
-          documents or send them by mail. We ensure that each certificate is
-          stored on the blockchain with encrypted information, protecting the
-          integrity and security of the records and minimizing fraud risks.
+        <div className="mt-4 w-full text-right md:w-4/6 lg:w-1/2">
+          By tokenizing diplomas, certificates, and educational accomplishments, we help teachers
+          and students build an authentic, verifiable digital portfolio that provides a
+          comprehensive view of their skills and knowledge. Syncible reduces the time to issue
+          certificates, printing costs, and educational fees, while eliminating the need to retrieve
+          physical documents or send them by mail. We ensure that each certificate is stored on the
+          blockchain with encrypted information, protecting the integrity and security of the
+          records and minimizing fraud risks.
         </div>
       </div>
     </div>

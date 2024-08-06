77584bd8e65a5ac02f210e0f2dc591819238a7e5
Truong Tuoc Phi <truongphi754@gmail.com>
2024-08-06 16:05:30 +0700

    feat: update

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,15 +1,22 @@
 import { ReactNode } from 'react';
 
 import ClientSessionProvider from '@/components/layout/ClientSessionProvider';
+import { headers } from 'next/headers';
+import { cookieToInitialState } from 'wagmi';
+import { config } from '@/config';
+import Web3ModalProvider from '@/context';
 
 interface AdminLayoutProps {
   children: ReactNode;
 }
 
 const AdminLayout = ({ children }: AdminLayoutProps) => {
+  const initialState = cookieToInitialState(config, headers().get('cookie'));
   return (
     <ClientSessionProvider>
-      <div>{children}</div>
+      <Web3ModalProvider initialState={initialState}>
+        <div>{children}</div>
+      </Web3ModalProvider>
     </ClientSessionProvider>
   );
 };
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -7,9 +7,16 @@ import { auth, onAuthStateChanged } from '@/lib/firebase';
 import UserInfo from '@/components/pages/admin/UserInfo';
 import { useRouter } from 'next/navigation';
 
+import ButtonPrimary from '@/components/common/button/ButtonPrimary';
+import { useWeb3Modal } from '@web3modal/wagmi/react';
+import { useAccount } from 'wagmi';
+
 const AdminDashboard = () => {
   const [user, setUser] = useState<any>(null);
-  const router = useRouter(); // Khởi tạo router để điều hướng
+  const router = useRouter();
+  const { open } = useWeb3Modal();
+
+  const { address, isConnected } = useAccount();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user: any) => {
@@ -62,14 +69,26 @@ const AdminDashboard = () => {
             <FaSearch className="absolute left-3 top-2 mt-1 text-gray-500" />
           </div>
 
-          <div className="flex items-center space-x-4">
-            {user ? (
-              <UserInfo user={user} />
-            ) : (
-              <a href="/login" className="text-blue-500 hover:text-blue-600">
-                Login
-              </a>
-            )}
+          <div className="flex gap-5">
+            <div className="flex items-center space-x-4">
+              {user ? (
+                <UserInfo user={user} />
+              ) : (
+                <a href="/login" className="text-blue-500 hover:text-blue-600">
+                  Login
+                </a>
+              )}
+            </div>
+
+            <div className="flex items-center gap-2 md:gap-4">
+              <ButtonPrimary onClick={() => open()}>
+                {isConnected && address ? (
+                  `${address.slice(0, 4)}...${address.slice(-6)}`
+                ) : (
+                  <div>Connect</div>
+                )}
+              </ButtonPrimary>
+            </div>
           </div>
         </div>
 

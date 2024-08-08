3e6f8836b030aa797a63d6b86709534e09c8c07c
Truong Tuoc Phi <truongphi754@gmail.com>
2024-08-08 16:50:25 +0700

    feat: update customized

---
+++ src/app/admin/customized/page.tsx
@@ -0,0 +1,12 @@
+import { HiTemplate } from 'react-icons/hi';
+
+export default function page() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <HiTemplate className="text-7xl text-gray-500" />
+        <div className="text-lg font-semibold text-gray-500">No Item</div>
+      </div>
+    </div>
+  );
+}

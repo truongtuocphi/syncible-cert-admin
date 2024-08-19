d94778018d69dcadce7a46430122e7cb3f5e4b57
Truong Tuoc Phi <truongphi754@gmail.com>
2024-08-19 15:34:39 +0700

    feat: update mintNFT

--- src/app/admin/mintnft/page.tsx
+++ src/app/admin/mintnft/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import { useState } from 'react';
-
 import CreateNFT from '@/components/pages/experience/CreateNFT';
 
 const Experience = () => {
-  const [templateData, setTemplateData] = useState<any>(null);
-
   return (
     <>
-      <CreateNFT templateData={templateData} />
+      <CreateNFT />
     </>
   );
 };

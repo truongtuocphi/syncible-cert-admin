48b786f822543c6fa2c7ec2df99748186cbf4053
Truong Tuoc Phi <truongphi754@gmail.com>
2024-09-05 10:08:52 +0700

    feat: update

--- src/components/pages/admin/Modal.tsx
+++ src/components/pages/admin/Modal.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 
+import { IoClose } from 'react-icons/io5';
+
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -23,26 +25,30 @@ const Modal = ({ isOpen, onClose }: ModalProps) => {
 
         <div className="mt-4 grid grid-cols-1 gap-6 md:grid-cols-2">
           {/* Card 1 */}
-          <div className="rounded-lg border p-4 text-center shadow-lg">
-            <div className="mx-auto text-4xl text-gray-500">📄</div>
-            <h3 className="mt-4 text-lg font-bold">Tạo chứng chỉ đơn lẻ</h3>
-            <p className="mt-2 line-clamp-3 text-sm">
-              Mục này được thiết kế cho việc cung cấp bằng cấp đơn lẻ dành cho một người.
-            </p>
-            <button className="mt-4 rounded-lg bg-teal-500 px-4 py-2 text-white">
+          <div className="flex flex-col justify-between rounded-lg border p-4 text-center shadow-lg">
+            <div>
+              <div className="mx-auto text-4xl text-gray-500">📄</div>
+              <h3 className="mt-4 text-lg font-bold">Tạo chứng chỉ đơn lẻ</h3>
+              <p className="mt-2 line-clamp-3 text-sm">
+                Mục này được thiết kế cho việc cung cấp bằng cấp đơn lẻ dành cho một người.
+              </p>
+            </div>
+            <button className="mt-4 rounded-full bg-blue-500 px-4 py-2 text-white">
               Tạo chứng chỉ
             </button>
           </div>
 
           {/* Card 2 */}
-          <div className="rounded-lg border p-4 text-center shadow-lg">
-            <div className="mx-auto text-4xl text-gray-500">📑</div>
-            <h3 className="mt-4 text-lg font-bold">Tạo chứng chỉ hàng loạt</h3>
-            <p className="mt-2 line-clamp-3 text-sm">
-              Mục này được thiết kế cho việc cung cấp bằng cấp hàng loạt cho khoá học trên một học
-              viên.
-            </p>
-            <button className="mt-4 rounded-lg bg-teal-500 px-4 py-2 text-white">
+          <div className="flex flex-col justify-between rounded-lg border p-4 text-center shadow-lg">
+            <div>
+              <div className="mx-auto text-4xl text-gray-500">📑</div>
+              <h3 className="mt-4 text-lg font-bold">Tạo chứng chỉ hàng loạt</h3>
+              <p className="mt-2 line-clamp-3 text-sm">
+                Mục này được thiết kế cho việc cung cấp bằng cấp hàng loạt cho khoá học trên một học
+                viên.
+              </p>
+            </div>
+            <button className="mt-4 rounded-full bg-blue-500 px-4 py-2 text-white">
               Tạo chứng chỉ
             </button>
           </div>
@@ -51,9 +57,9 @@ const Modal = ({ isOpen, onClose }: ModalProps) => {
         {/* Nút đóng */}
         <button
           onClick={onClose}
-          className="absolute right-2 top-2 text-gray-500 hover:text-gray-700"
+          className="absolute right-4 top-4 text-gray-500 hover:text-gray-700"
         >
-          ✖
+          <IoClose className="text-2xl" />
         </button>
       </div>
     </div>

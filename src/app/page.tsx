eb7f22b318279f2026c011b54d5d7bf3a5453a97
Trung Tran <pochanwa@gmail.com>
2024-09-15 19:05:21 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,11 +28,11 @@ const Page = () => {
         <Navbar />
       </div>
       <div className="relative z-20 flex flex-col items-center gap-10 overflow-hidden">
-        <div className={`${montserrat.className} relative w-full max-w-[90rem] `}>
-          <div className="flex min-h-dvh flex-col px-4 py-4 md:px-8 md:py-10 xl:px-[6.5rem] xl:py-12">
-            <div className="relative flex h-full w-full flex-grow flex-col justify-center md:px-6 lg:flex-row lg:pt-0">
+        <div className={`${montserrat.className} w-full max-w-[90rem]`}>
+          <div className="flex flex-col px-4 pb-4 sm:min-h-dvh md:px-8 md:pb-10 xl:px-32 ">
+            <div className="mt-24 flex h-full w-full flex-grow flex-col justify-center md:mt-[8.25rem] lg:mt-40  lg:flex-row xl:mt-44">
               <div className="flex items-center justify-center lg:basis-1/2">
-                <div className="flex h-[20rem] animate-swipe-up-fadein flex-col items-center justify-center gap-8 antialiased lg:h-[30rem] lg:items-start">
+                <div className="flex min-h-[20rem] animate-swipe-up-fadein flex-col items-center justify-center gap-6 antialiased sm:gap-8 lg:h-[30rem] lg:items-start">
                   <div
                     className={`text-md col-span-1 w-full text-wrap text-left text-[2.5rem] font-bold leading-none sm:text-center lg:text-left lg:text-5xl lg:leading-[4rem]`}
                   >
@@ -42,7 +42,7 @@ const Page = () => {
                     Using blockchain technology and NFTs, Syncible enhance your institution’s
                     reputation and revolutionizes the way academic achievements are recognized.
                   </div>
-                  <div className="self-center lg:self-start">
+                  <div className="w-full self-center sm:w-auto lg:self-start">
                     <Link href="/contact">
                       <Button className="w-full rounded-[1.25rem] bg-primary-50 px-10 py-7 text-base shadow-combinedShadow1 hover:bg-primary-40">
                         Contact us
@@ -51,8 +51,8 @@ const Page = () => {
                   </div>
                 </div>
               </div>
-              <div className="relative h-fit w-full animate-swipe-up-fadein self-center  lg:basis-1/2">
-                <div className="absolute left-auto top-auto h-fit w-full lg:left-[-19%] lg:top-[-19%] lg:h-[130%] lg:w-[130%] xl:left-[-27%] xl:top-[-30%] xl:h-[160%] xl:w-[160%]">
+              <div className="relative h-fit w-full animate-swipe-up-fadein self-center lg:basis-1/2">
+                <div className="md:scale-80 absolute left-1/2 top-1/2 hidden h-full w-full -translate-x-1/2 -translate-y-1/2 scale-50 sm:scale-90 md:block lg:scale-110">
                   <Image
                     src={BannerCertificate}
                     alt="Certificate"
@@ -60,18 +60,18 @@ const Page = () => {
                     className="block h-full w-full object-contain"
                   />
                 </div>
-                <div className="hidden md:block ">
+                <div className="block md:invisible">
                   <Image
                     src={BannerCertificate}
                     alt="Certificate placeholder"
-                    className="invisible min-h-[30rem] w-full object-cover md:min-h-full"
+                    className="min-h-[30rem] w-full object-contain md:min-h-full"
                     priority
                   />
                 </div>
               </div>
             </div>
             <div className="flex flex-col gap-8 pb-10">
-              <div className="text-md font-bold md:text-2xl lg:text-4xl">Powered By</div>
+              <div className="text-md text-[2rem] font-bold lg:text-4xl">Powered By</div>
               <div className="flex gap-20">
                 <div className="h-11 w-44">
                   <EthereumLogoSVG className="h-full w-full" />
@@ -99,7 +99,7 @@ const Page = () => {
           <div className="absolute -left-[170%] -top-[15%] -z-10 h-[125rem] w-[125rem] sm:-top-[15%] sm:left-[-40%]">
             <LightBlueGradientEllipse className="h-full w-full" />
           </div>
-          <div className="absolute -right-[130%] top-1/2 -z-10 h-[115rem] w-[115rem] -translate-y-[70%] sm:-right-1/2 sm:-translate-y-[40%]">
+          <div className="absolute -right-[130%] top-1/2 -z-10 h-[115rem] w-[115rem] -translate-y-[70%] sm:-right-1/2 sm:-translate-y-[35%]">
             <LightBlueGradientEllipse className="h-full w-full" />
           </div>
           <div className="absolute -bottom-[10%] -left-[150%] -z-10 h-[125rem] w-[125rem] sm:-bottom-1/2 sm:-left-1/2 sm:-translate-y-[95%]">

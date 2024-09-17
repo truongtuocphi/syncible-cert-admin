dc28504695554c994a0da386c26553fff9b48c41
Truong Tuoc Phi <truongphi754@gmail.com>
2024-09-17 18:17:10 +0700

    feat: update

---
+++ src/components/pages/admin/editor/PhotoEditorSDK.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable no-console */
+import React, { useEffect } from 'react';
+
+import { UIEvent, PhotoEditorSDKUI } from 'photoeditorsdk';
+
+const PhotoEditorSDK = () => {
+  useEffect(() => {
+    const initEditor = async () => {
+      try {
+        const editor = await PhotoEditorSDKUI.init({
+          container: '#editor',
+          image:
+            'https://fuchsia-fancy-orangutan-869.mypinata.cloud/ipfs/QmTSo1QyvYhb6csz2p46mkdVz7ZoHMwepDgDBTzkLeJBjh',
+          license: 'DULee_tRwe4jZOZg8kE2UYZcd229Ol5joYQSvfObuVwuf5uZrk3s5cp9p9npblwY',
+          theme: 'light',
+        });
+        console.log('PhotoEditorSDK for Web is ready!');
+        editor.on(UIEvent.EXPORT, (imageSrc) => {
+          console.log('Exported ', imageSrc);
+        });
+      } catch (error) {
+        console.error('Failed to initialize PhotoEditorSDK:', error);
+      }
+    };
+
+    initEditor();
+  }, []);
+
+  return (
+    <div
+      id="editor"
+      style={{
+        width: '100%',
+        height: '80vh',
+        margin: 'auto',
+        backgroundColor: '#f0f0f0',
+        border: '1px solid #ccc',
+        position: 'relative',
+      }}
+    />
+  );
+};
+
+export default PhotoEditorSDK;

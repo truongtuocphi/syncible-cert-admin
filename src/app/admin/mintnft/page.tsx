58732b39c29fad6599b999ce771e0d7e6ecd8ff2
Truong Tuoc Phi <truongphi754@gmail.com>
2024-09-19 18:03:23 +0700

    feat: update

--- src/app/admin/mintnft/page.tsx
+++ src/app/admin/mintnft/page.tsx
@@ -20,7 +20,6 @@ import { db, ref, get } from '@/lib/firebase';
 import { uploadMetadata } from '@/lib/pinata';
 import { Collection } from '@/types/function';
 import { saveMintData } from '@/utils/saveMintData';
-import { uploadImageToPinata } from '@/utils/uploadImageToPinata';
 
 const Experience = () => {
   const pathname = useSearchParams();
@@ -107,18 +106,17 @@ const Experience = () => {
     setBannerImage(null);
   };
 
-  const handleImageBannerChange = async (
+  const handleImageBannerChange = (
     e: React.ChangeEvent<HTMLInputElement>,
     setImage: React.Dispatch<React.SetStateAction<string | null>>
   ) => {
     const file = e.target.files?.[0];
     if (file) {
-      try {
-        const imageUrl = await uploadImageToPinata(file);
-        setImage(imageUrl);
-      } catch (error) {
-        console.error('Error handling image banner change', error);
-      }
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
     }
   };
 
@@ -180,7 +178,7 @@ const Experience = () => {
               data.fullname,
               data.certificateNumber,
               tokenURI,
-              [issuedDate, bannerImage],
+              [issuedDate, 'https://example.com/template/1'],
             ];
           }
         )

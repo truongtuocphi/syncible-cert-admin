4b23c2737a477fe2fc23ff45912477c675e07fdc
Truong Tuoc Phi <truongphi754@gmail.com>
2024-09-23 16:22:49 +0700

    feat: update max width

--- src/app/[locale]/blogs/page.tsx
+++ src/app/[locale]/blogs/page.tsx
@@ -97,87 +97,43 @@ export default function Blogs() {
   };
 
   return (
-    <div className="relative min-h-screen max-w-[90rem]">
+    <div className="relative min-h-screen">
       <div className="fixed top-0 z-30 w-screen md:mt-6">
         <Navbar />
       </div>
       <div className={`${montserrat.className}  z-20 flex flex-col items-center gap-10`}>
-        <div className="flex h-full w-full justify-center pt-24 md:pt-[8.25rem] lg:pt-40 xl:pt-44">
-          <div className="flex flex-col px-4 pb-4 md:px-8 md:pb-10 xl:px-32">
-            <div className="flex flex-col items-center gap-10">
-              <div className="text-center text-2xl font-bold md:text-3xl lg:text-5xl">
-                {t('header')}
-              </div>
-              <div className="w-full">
-                <SyncibleBanner className="aspect-[24/9] h-full w-full" />
-              </div>
-            </div>
-            <div className="flex flex-col gap-12 md:flex-row">
-              <div className="w-full overflow-hidden rounded-xl bg-white/50 text-[#A2A3A9] md:hidden">
-                <Button
-                  className={clsx(
-                    'flex w-full justify-between rounded-lg bg-white/50 px-4 py-2 text-left text-[#2C2C2C] hover:bg-white/50 hover:text-[#2C2C2C]',
-                    { 'rounded-b-none border-b': toggleBlogNav }
-                  )}
-                  onClick={handleToggleBlogNav}
-                >
-                  {t('nav_select_placeholder.label')}
-                  <div
-                    className={clsx('transition-transform', {
-                      'rotate-180 duration-300': toggleBlogNav,
-                    })}
-                  >
-                    <ChevronDown />
-                  </div>
-                </Button>
-                <div className={clsx('flex flex-col gap-2 px-4 py-2', { hidden: !toggleBlogNav })}>
-                  {keys.map((key) => (
-                    <Link
-                      key={key}
-                      href={t(`navigation.${key}.href`)}
-                      className={clsx(
-                        'hover:text-[#2C2C2C]',
-                        pathname === t(`navigation.${key}.href`) && 'text-[#2C2C2C]'
-                      )}
-                      onClick={() => setToggleBlogNav(false)}
-                    >
-                      {t(`navigation.${key}.label`)}
-                    </Link>
-                  ))}
+        <div className="w-full max-w-[90rem]">
+          <div className="flex h-full w-full justify-center pt-24 md:pt-[8.25rem] lg:pt-40 xl:pt-44">
+            <div className="flex flex-col px-4 pb-4 md:px-8 md:pb-10 xl:px-32">
+              <div className="flex flex-col items-center gap-10">
+                <div className="text-center text-2xl font-bold md:text-3xl lg:text-5xl">
+                  {t('header')}
+                </div>
+                <div className="w-full">
+                  <SyncibleBanner className="aspect-[24/9] h-full w-full" />
                 </div>
               </div>
-              <div className="hidden basis-1/4 md:block">
-                <div className="flex h-full flex-col gap-8">
-                  <div className="flex flex-col gap-6">
-                    <div className="flex items-center gap-4">
-                      <div>
-                        <Avatar className="h-16 w-16">
-                          <AvatarImage src="/SmugFace.png" alt="" />
-                          <AvatarFallback>SC</AvatarFallback>
-                        </Avatar>
-                      </div>
-                      <div>
-                        <div className="text-lg font-bold">{t('author_profile.name')}</div>
-                        <div className="text-base font-medium text-[#A2A3A9]">
-                          {t('author_profile.role')}
-                        </div>
-                      </div>
+              <div className="flex flex-col gap-12 md:flex-row">
+                <div className="w-full overflow-hidden rounded-xl bg-white/50 text-[#A2A3A9] md:hidden">
+                  <Button
+                    className={clsx(
+                      'flex w-full justify-between rounded-lg bg-white/50 px-4 py-2 text-left text-[#2C2C2C] hover:bg-white/50 hover:text-[#2C2C2C]',
+                      { 'rounded-b-none border-b': toggleBlogNav }
+                    )}
+                    onClick={handleToggleBlogNav}
+                  >
+                    {t('nav_select_placeholder.label')}
+                    <div
+                      className={clsx('transition-transform', {
+                        'rotate-180 duration-300': toggleBlogNav,
+                      })}
+                    >
+                      <ChevronDown />
                     </div>
-                    <div className="w-[70%] border-t-[1px]"></div>
-                    <div className="flex flex-col gap-2">
-                      <div className="text-base font-medium text-[#A2A3A9]">
-                        {t('blog_info.date_created.label')}
-                      </div>
-                      <div className="text-lg font-medium">{t('blog_info.date_created.value')}</div>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <div className="text-base font-medium text-[#A2A3A9]">
-                        {t('blog_info.read_time.label')}
-                      </div>
-                      <div className="text-lg font-medium">{t('blog_info.read_time.value')}</div>
-                    </div>
-                  </div>
-                  <div className="sticky top-[9rem] flex flex-col gap-2 text-base font-bold text-[#A2A3A9] 2xl:text-lg">
+                  </Button>
+                  <div
+                    className={clsx('flex flex-col gap-2 px-4 py-2', { hidden: !toggleBlogNav })}
+                  >
                     {keys.map((key) => (
                       <Link
                         key={key}
@@ -186,33 +142,32 @@ export default function Blogs() {
                           'hover:text-[#2C2C2C]',
                           pathname === t(`navigation.${key}.href`) && 'text-[#2C2C2C]'
                         )}
+                        onClick={() => setToggleBlogNav(false)}
                       >
                         {t(`navigation.${key}.label`)}
                       </Link>
                     ))}
                   </div>
                 </div>
-              </div>
-              <div className="basis-3/4">
-                <div className="flex flex-col gap-8">
-                  <div className="flex flex-col gap-6 md:hidden">
-                    <div className="flex items-center gap-4">
-                      <div>
-                        <Avatar className="h-16 w-16">
-                          <AvatarImage src="/SmugFace.png" />
-                          <AvatarFallback>SC</AvatarFallback>
-                        </Avatar>
-                      </div>
-                      <div>
-                        <div className="text-lg font-bold">{t('author_profile.name')}</div>
-                        <div className="text-base font-medium text-[#A2A3A9]">
-                          {t('author_profile.role')}
+                <div className="hidden basis-1/4 md:block">
+                  <div className="flex h-full flex-col gap-8">
+                    <div className="flex flex-col gap-6">
+                      <div className="flex items-center gap-4">
+                        <div>
+                          <Avatar className="h-16 w-16">
+                            <AvatarImage src="/SmugFace.png" alt="" />
+                            <AvatarFallback>SC</AvatarFallback>
+                          </Avatar>
+                        </div>
+                        <div>
+                          <div className="text-lg font-bold">{t('author_profile.name')}</div>
+                          <div className="text-base font-medium text-[#A2A3A9]">
+                            {t('author_profile.role')}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="w-full border-t-[1px]"></div>
-                    <div className="flex gap-4 ">
-                      <div className="flex w-full flex-col gap-2">
+                      <div className="w-[70%] border-t-[1px]"></div>
+                      <div className="flex flex-col gap-2">
                         <div className="text-base font-medium text-[#A2A3A9]">
                           {t('blog_info.date_created.label')}
                         </div>
@@ -220,15 +175,68 @@ export default function Blogs() {
                           {t('blog_info.date_created.value')}
                         </div>
                       </div>
-                      <div className="flex w-full flex-col gap-2">
+                      <div className="flex flex-col gap-2">
                         <div className="text-base font-medium text-[#A2A3A9]">
                           {t('blog_info.read_time.label')}
                         </div>
                         <div className="text-lg font-medium">{t('blog_info.read_time.value')}</div>
                       </div>
                     </div>
+                    <div className="sticky top-[9rem] flex flex-col gap-2 text-base font-bold text-[#A2A3A9] 2xl:text-lg">
+                      {keys.map((key) => (
+                        <Link
+                          key={key}
+                          href={t(`navigation.${key}.href`)}
+                          className={clsx(
+                            'hover:text-[#2C2C2C]',
+                            pathname === t(`navigation.${key}.href`) && 'text-[#2C2C2C]'
+                          )}
+                        >
+                          {t(`navigation.${key}.label`)}
+                        </Link>
+                      ))}
+                    </div>
                   </div>
-                  <div>{Content && <Content components={overrideComponents} />}</div>
+                </div>
+                <div className="basis-3/4">
+                  <div className="flex flex-col gap-8">
+                    <div className="flex flex-col gap-6 md:hidden">
+                      <div className="flex items-center gap-4">
+                        <div>
+                          <Avatar className="h-16 w-16">
+                            <AvatarImage src="/SmugFace.png" />
+                            <AvatarFallback>SC</AvatarFallback>
+                          </Avatar>
+                        </div>
+                        <div>
+                          <div className="text-lg font-bold">{t('author_profile.name')}</div>
+                          <div className="text-base font-medium text-[#A2A3A9]">
+                            {t('author_profile.role')}
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-full border-t-[1px]"></div>
+                      <div className="flex gap-4 ">
+                        <div className="flex w-full flex-col gap-2">
+                          <div className="text-base font-medium text-[#A2A3A9]">
+                            {t('blog_info.date_created.label')}
+                          </div>
+                          <div className="text-lg font-medium">
+                            {t('blog_info.date_created.value')}
+                          </div>
+                        </div>
+                        <div className="flex w-full flex-col gap-2">
+                          <div className="text-base font-medium text-[#A2A3A9]">
+                            {t('blog_info.read_time.label')}
+                          </div>
+                          <div className="text-lg font-medium">
+                            {t('blog_info.read_time.value')}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div>{Content && <Content components={overrideComponents} />}</div>
+                  </div>
                 </div>
               </div>
             </div>

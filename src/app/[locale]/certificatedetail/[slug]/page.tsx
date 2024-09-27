b93cea5a3843d59cf7783c371212e1fb738cb015
Truong Tuoc Phi <truongphi754@gmail.com>
2024-09-27 18:13:27 +0700

    feat: update

--- src/app/[locale]/certificatedetail/[slug]/page.tsx
+++ src/app/[locale]/certificatedetail/[slug]/page.tsx
@@ -18,14 +18,15 @@ export default function CertificateDetail({ params }: { params: { slug: string }
         <div className="relative text-black">
           <Footer />
         </div>
-      </div>
-      <div className="absolute bottom-0 left-0 right-0 top-0 flex h-full w-full flex-col items-center overflow-hidden">
-        <div className="relative mx-auto h-full w-full max-w-[90rem]">
-          <div className="absolute -left-1/2 -top-1/2 -z-10 w-[125rem] sm:-top-[5%] sm:left-[-40%] sm:w-[150%] sm:-translate-x-[20%] sm:-translate-y-[50%]">
-            <LightBlueGradientEllipse className="h-full w-full" />
-          </div>
-          <div className="absolute -bottom-1/2 -right-1/2 -z-10 w-[125rem] sm:-translate-y-[20%] sm:translate-x-[20%]">
-            <LightBlueGradientEllipse className="h-full w-full" />
+
+        <div className="absolute bottom-0 left-0 right-0 top-0 flex h-full w-full flex-col items-center overflow-hidden">
+          <div className="relative mx-auto h-screen w-full max-w-[90rem]">
+            <div className="absolute -left-1/2 -top-1/2 -z-10 w-[125rem] sm:-top-[5%] sm:left-[-40%] sm:w-[150%] sm:-translate-x-[20%] sm:-translate-y-[50%]">
+              <LightBlueGradientEllipse className="h-full w-full" />
+            </div>
+            <div className="absolute -bottom-1/2 -right-1/2 -z-10 w-[125rem] sm:-translate-y-[20%] sm:translate-x-[20%]">
+              <LightBlueGradientEllipse className="h-full w-full" />
+            </div>
           </div>
         </div>
       </div>

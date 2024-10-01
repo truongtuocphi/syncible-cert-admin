11b0eb113f10739113278ee8668189db954d56b0
Truong Tuoc Phi <truongphi754@gmail.com>
2024-10-01 17:17:22 +0700

    feat: update

--- src/components/pages/Home/SectionBlogs.tsx
+++ src/components/pages/Home/SectionBlogs.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Autoplay from 'embla-carousel-autoplay';
-import { useTranslations } from 'next-intl';
+import { useTranslations, useLocale } from 'next-intl';
 
 import ArrowNarrowRight from '@/assets/icons/arrow-narrow-right.svg';
 import BlogCard from '@/components/common/card/BlogCard';
@@ -15,25 +15,34 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { fetchDataFromWP } from '@/utils/fetchDataFromWordPress';
 
-
 export default function SectionBlogs() {
   const t = useTranslations('HomePage.blogs_section');
   const [posts, setPosts] = useState<ArticleEntry[]>([]);
 
+  const locale = useLocale();
+
   useEffect(() => {
     async function fetchPosts() {
-      const response = await fetchDataFromWP('https://admin.syncible.io/wp-json/wp/v2/posts');
+      const tagId = locale === 'en' ? 6 : 7;
+
+      const response = await fetchDataFromWP(
+        `https://admin.syncible.io/wp-json/wp/v2/posts?tags=${tagId}`
+      );
       const mappedPosts = await Promise.all(
         response.map(async (post: any) => {
           // Fetch featured media (banner image) if available
           let bannerImg = '';
           if (post.featured_media) {
-            const mediaResponse = await fetchDataFromWP(`https://admin.syncible.io/wp-json/wp/v2/media/${post.featured_media}`);
+            const mediaResponse = await fetchDataFromWP(
+              `https://admin.syncible.io/wp-json/wp/v2/media/${post.featured_media}`
+            );
             bannerImg = mediaResponse.source_url || ''; // Ensure a default empty string if no image
           }
 
           // Fetch author details
-          const authorResponse = await fetchDataFromWP(`https://admin.syncible.io/wp-json/wp/v2/users/${post.author}`);
+          const authorResponse = await fetchDataFromWP(
+            `https://admin.syncible.io/wp-json/wp/v2/users/${post.author}`
+          );
           const author = {
             name: authorResponse.name,
             avatar: authorResponse.avatar_urls['96'], // Use the 96px avatar size
@@ -52,7 +61,7 @@ export default function SectionBlogs() {
       setPosts(mappedPosts);
     }
     fetchPosts();
-  }, []);
+  }, [locale]);
 
   return (
     <div className="w-full max-w-[90rem]" id="blogs">
@@ -78,7 +87,6 @@ export default function SectionBlogs() {
         >
           <div className="flex flex-col justify-center">
             <CarouselContent className="pb-10 pt-7 md:-ml-6">
-              
               {posts.map((entry, index) => (
                 <CarouselItem key={index} className="basis-full md:basis-1/2 md:pl-6 lg:basis-1/3">
                   <BlogCard entry={entry} />

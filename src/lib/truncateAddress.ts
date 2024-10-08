caf0dabb79bbc6b837fe598a4c55a4d92a421d4b
Truong Tuoc Phi <truongphi754@gmail.com>
2024-10-08 10:48:17 +0700

    feat: update

--- src/lib/truncateAddress.ts
+++ src/lib/truncateAddress.ts
@@ -1,3 +1,3 @@
 export default function truncateAddress(address: string, startLength = 6, endLength = 4) {
-  return `${address.slice(0, startLength)}...${address.slice(-endLength)}`;
+  return `${address?.slice(0, startLength)}...${address?.slice(-endLength)}`;
 }

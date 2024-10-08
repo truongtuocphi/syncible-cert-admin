4601207304a4f0f4168a77d88d58ce28f8ccffc1
Truong Tuoc Phi <truongphi754@gmail.com>
2024-10-08 14:35:17 +0700

    feat: update

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -3,7 +3,7 @@
 import { IConfig } from 'next-sitemap';
 
 const config: IConfig = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://www.syncible.com', // URL trang web của bạn
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://www.syncible.io', // URL trang web của bạn
   generateRobotsTxt: true, // Tạo robots.txt tự động
   sitemapSize: 7000, // Giới hạn số URL trong một sitemap (mặc định 5000)
   changefreq: 'daily', // Tần suất Google nên crawl trang web

3449f2c9da0773d8509c23c113f218435ceb5c59
Truong Tuoc Phi <truongphi754@gmail.com>
2024-10-08 15:47:00 +0700

    feawt: udpate

--- src/app/[locale]/oauth/callback/page.tsx
+++ src/app/[locale]/oauth/callback/page.tsx
@@ -103,7 +103,7 @@ const Page = () => {
             fist_name: userInfoData?.data?.first_name || '',
             name: userInfoData?.data?.first_name || '',
             email,
-            avatar: userInfoData?.data?.avatar || '',
+            avatar: '',
             createdAt: new Date().toISOString(),
           });
           setTimeout(() => {

9fb3042cff2e8a8f798e710825841b8e125cb8ec
truongphi <truongphi754@gmail.com>
2024-10-17 15:08:43 +0700

    feat: update

--- src/app/[locale]/(landing)/page.tsx
+++ src/app/[locale]/(landing)/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
@@ -19,18 +19,43 @@ import BannerCertificate from '../../../../public/cert_example.png';
 import EthereumLogoSVG from '../../../../public/ethereum-logo.svg';
 import PolygonLogoSVG from '../../../../public/polygon-logo.svg';
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import ButtonPrimary from '@/components/common/button/ButtonPrimary';
+
+import { useForm, ValidationError } from '@formspree/react';
+
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/material.css';
+import '../../../style/phoneInput.css';
+import imageSuccessfully from '../../../../public/successfully.png';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const Page = () => {
+  const [phone, setPhone] = useState('');
+  const [state, handleSubmit] = useForm('mkgnnqbd');
+
   const t = useTranslations('HomePage.title_section');
+
+  const handleChange = (value: string) => {
+    setPhone(value);
+  };
+
   return (
     <>
       <div
         className={`${montserrat.className} relative z-20 flex flex-col items-center gap-10 overflow-hidden`}
       >
         <div className="w-full max-w-[90rem]">
-        {/* <div className="w-full"> */}
+          {/* <div className="w-full"> */}
           <div className="flex flex-col justify-center px-4 pb-4 sm:min-h-dvh md:px-8 md:pb-10 xl:px-32">
-            <div className="flex h-full w-full flex-col justify-center mt-24 md:mt-[8.25rem] lg:flex-row">
+            <div className="mt-24 flex h-full w-full flex-col justify-center md:mt-[8.25rem] lg:flex-row">
               <div className="flex items-center justify-center lg:basis-1/2 lg:pr-6">
                 <div className="flex min-h-[20rem] animate-swipe-up-fadein flex-col items-center justify-center gap-6 antialiased sm:gap-8 lg:h-[30rem] lg:items-start">
                   <div
@@ -42,10 +67,7 @@ const Page = () => {
                     {t('label_2')}
                   </div>
                   <div className="w-full self-center sm:w-auto lg:self-start">
-                    <Link href={t('contact_button.href')}>
-                      {/* <Button className="w-full rounded-[1.25rem] bg-primary-50 px-10 py-7 text-base shadow-combinedShadow1 hover:bg-primary-40">
-                        Contact us
-                      </Button> */}
+                    {/* <Link href={t('contact_button.href')}>
                       <Button
                         className={`
                           relative z-0 flex w-full items-center gap-2 overflow-hidden rounded-[1.25rem] bg-primary-50
@@ -66,7 +88,133 @@ const Page = () => {
                       >
                         <span>{t('contact_button.label')}</span>
                       </Button>
-                    </Link>
+                    </Link> */}
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <Button
+                          className={`
+                          relative z-0 flex w-full items-center gap-2 overflow-hidden rounded-[1.25rem] bg-primary-50
+                          px-10 py-7 text-base font-semibold
+                          uppercase transition-all duration-500
+        
+                          before:absolute before:inset-0
+                          before:-z-10 before:translate-x-[150%]
+                          before:translate-y-[150%] before:scale-[2.5]
+                          before:rounded-[100%] before:bg-primary-40
+                          before:transition-transform before:duration-1000
+                          before:content-[""]
+
+                          hover:scale-105
+                          hover:before:translate-x-[0%]
+                          hover:before:translate-y-[0%]
+                          active:scale-95`}
+                        >
+                          <span>{t('contact_button.label')}</span>
+                        </Button>
+                      </DialogTrigger>
+                      <DialogContent className="scrollable-content h-full w-screen overflow-y-auto !rounded-none pb-0 md:!rounded-3xl lg:h-[95%] lg:max-w-[576px]">
+                        <DialogHeader className="mt-16">
+                          <DialogTitle className="text-center text-4xl">Contact us</DialogTitle>
+                          <DialogDescription className="text-center text-lg">
+                            Tell us what we can help you
+                          </DialogDescription>
+                        </DialogHeader>
+                        {!state.succeeded ? (
+                          <form onSubmit={handleSubmit} className="h-fit space-y-5">
+                            <div className="space-y-2">
+                              <label className="font-bold text-gray-900">Full name*</label>
+                              <input
+                                id="full-name"
+                                name="fullname"
+                                type="text"
+                                className="block w-full rounded-3xl border-[1px] border-gray-400 p-4"
+                                placeholder="Your full name"
+                                required
+                              />
+                            </div>
+                            <div className="space-y-2">
+                              <label className="font-bold text-gray-900">Email*</label>
+                              <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                className="block w-full rounded-3xl border-[1px] border-gray-400 p-4"
+                                placeholder="Your email"
+                                required
+                              />
+                            </div>
+                            <div className="space-y-2">
+                              <label className="font-bold text-gray-900">Phone number*</label>
+                              <PhoneInput
+                                country={'vn'}
+                                value={phone}
+                                onChange={handleChange}
+                                inputStyle={{
+                                  border: '1px solid #A2A3A9',
+                                  borderRadius: '20px',
+                                  width: '100%',
+                                  padding: '16px 16px 16px 55px',
+                                }}
+                                enableSearch={true}
+                                searchPlaceholder="Search country..."
+                              />
+                            </div>
+                            <div className="space-y-2">
+                              <label className="font-bold text-gray-900">
+                                Company or institution name*
+                              </label>
+                              <input
+                                id="company"
+                                name="company"
+                                type="text"
+                                className="block w-full rounded-3xl border-[1px] border-gray-400 p-4"
+                                placeholder="Your company or organization name"
+                                required
+                              />
+                            </div>
+                            <div className="space-y-2">
+                              <label className="font-bold text-gray-900">
+                                Please describe your plan for using our platform
+                              </label>
+                              <textarea
+                                id="message"
+                                name="message"
+                                rows={6}
+                                className="block w-full rounded-3xl border-[1px] border-gray-400 p-4"
+                                placeholder="Type your message here"
+                                required
+                              />
+                            </div>
+                            <ButtonPrimary
+                              type="submit"
+                              disabled={state.submitting}
+                              className="w-full"
+                            >
+                              Submit
+                            </ButtonPrimary>
+                            <div className="text-red">
+                              <ValidationError errors={state.errors} />
+                            </div>
+                          </form>
+                        ) : (
+                          <DialogContent className="!rounded-3xl sm:max-w-[576px]">
+                            <DialogHeader className="mt-16">
+                              <DialogTitle className="text-center text-4xl">Contact us</DialogTitle>
+                              <DialogDescription className="text-center text-lg">
+                                Congratulations, you have submitted successfully.
+                              </DialogDescription>
+                            </DialogHeader>
+                            <div className="mt-10 flex w-full items-center justify-center">
+                              <Image
+                                src={imageSuccessfully}
+                                alt="image succesfully"
+                                className="h-[80%] w-[60%]"
+                              />
+                            </div>
+                          </DialogContent>
+                        )}
+                      </DialogContent>
+                    </Dialog>
                   </div>
                 </div>
               </div>
@@ -105,7 +253,7 @@ const Page = () => {
         </div>
 
         <div className="relative z-20 flex max-w-[90rem] flex-col items-center gap-8 sm:gap-40">
-        {/* <div className="relative z-20 flex flex-col items-center gap-8 sm:gap-40"> */}
+          {/* <div className="relative z-20 flex flex-col items-center gap-8 sm:gap-40"> */}
           <SectionAbout />
           <SectionWhatWeBelieve />
           <SectionOurVision />

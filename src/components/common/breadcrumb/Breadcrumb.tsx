a7453b9cab6d18ae45b1563d74972bfe8a4a41a3
Trung <pochanwa@gmail.com>
2024-10-18 14:59:39 +0700

    update

--- src/components/common/breadcrumb/Breadcrumb.tsx
+++ src/components/common/breadcrumb/Breadcrumb.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation';
 import { MdNavigateNext } from 'react-icons/md';
 
 import capitalizeFirstLetter from '@/utils/capitalizeFirstLetter';
-import { useCustomLabel } from '@/utils/labelCustomization';
+import { getCustomLabel } from '@/utils/labelCustomization';
 
 interface propData {
   displayName?: string;
@@ -20,63 +20,62 @@ const Breadcrumb = ({ displayName, nameCertificate }: propData) => {
   const isAdminMintNFTPath = pathname === '/admin/mintnft';
 
   // Generate labels for each path
-  const labels = paths.map((path) => useCustomLabel(path));
-
-  const breadcrumbItems = paths
-    .filter((path, index) => {
-      if (isAdminMintNFTPath && index === 0 && path.toLowerCase() === 'admin') {
-        return false;
-      }
-      return true;
-    })
-    .map((path, index) => {
-      const href = `/${paths.slice(0, index + 1).join('/')}`;
-      let label = '';
-
-      if (path.length === 42) {
-        label = displayName || 'Loading...';
-      }
-
-      if (path.length === 46 && nameCertificate) {
-        label = nameCertificate || 'Loading...';
-      } else {
-        // Use the pre-calculated label for each path
-        label = labels[index];
-      }
-      const truncatedLabel = label.length > 41 ? `${displayName}` : label;
-
-      return { label: truncatedLabel, href };
-    });
+  
 
   return (
     <nav aria-label="breadcrumb">
       <ol className="flex text-gray-400">
-        {breadcrumbItems.map((item, index) => (
-          <li key={item.href} className="flex items-center text-sm 2xl:text-base">
-            {index < breadcrumbItems.length - 1 ? (
-              <div className="flex items-center gap-2">
-                {item.label && (
-                  <>
-                    <Link
-                      href={item.href}
-                      className="line-clamp-1 text-sm hover:text-gray-800 2xl:text-base"
-                    >
-                      {capitalizeFirstLetter(item.label)}
-                    </Link>
-                    <MdNavigateNext className="mr-2 text-xl" />
-                  </>
+      {paths
+          .filter((path, index) => {
+            if (isAdminMintNFTPath && index === 0 && path.toLowerCase() === 'admin') {
+              return false;
+            }
+            return true;
+          })
+          .map((path, index) => {
+            const href = `/${paths.slice(0, index + 1).join('/')}`;
+
+            // Call `useCustomLabel` individually here, outside of any callback functions
+            let label = getCustomLabel(path);
+
+            // Handle specific conditions for path lengths
+            if (path.length === 42) {
+              label = displayName || 'Loading...';
+            }
+
+            if (path.length === 46 && nameCertificate) {
+              label = nameCertificate || 'Loading...';
+            }
+
+            const truncatedLabel = label.length > 41 ? `${displayName}` : label;
+
+            return (
+              <li key={href} className="flex items-center text-sm 2xl:text-base">
+                {index < paths.length - 1 ? (
+                  <div className="flex items-center gap-2">
+                    {truncatedLabel && (
+                      <>
+                        <Link
+                          href={href}
+                          className="line-clamp-1 text-sm hover:text-gray-800 2xl:text-base"
+                        >
+                          {capitalizeFirstLetter(truncatedLabel)}
+                        </Link>
+                        <MdNavigateNext className="mr-2 text-xl" />
+                      </>
+                    )}
+                  </div>
+                ) : (
+                  <Link
+                    href={href}
+                    className="text-sm font-bold text-gray-600 hover:text-gray-800 2xl:text-base"
+                  >
+                    {capitalizeFirstLetter(truncatedLabel)}
+                  </Link>
                 )}
-              </div>
-            ) : (
-              <Link
-                href={item.href}
-                className="text-sm font-bold text-gray-600 hover:text-gray-800 2xl:text-base"
-              >
-                {capitalizeFirstLetter(item.label)}
-              </Link>
-            )}
-          </li>
-        ))}
+              </li>
+            );
+          })}
       </ol>
     </nav>
   );

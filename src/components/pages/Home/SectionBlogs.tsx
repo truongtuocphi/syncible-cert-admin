9da7b7ed0c5f205827bdbb13a691c56ef49842f7
truongphi <truongphi754@gmail.com>
2024-11-28 10:37:13 +0700

    update components

--- src/components/pages/Home/SectionBlogs.tsx
+++ src/components/pages/Home/SectionBlogs.tsx
@@ -1,5 +1,4 @@
 'use client';
-import Autoplay from 'embla-carousel-autoplay';
 import { useTranslations, useLocale } from 'next-intl';
 
 import ArrowNarrowRight from '@/assets/icons/arrow-narrow-right.svg';
@@ -14,7 +13,6 @@ import {
 import { Link } from '@/i18n/routing';
 import { useEffect, useState } from 'react';
 import { fetchDataFromWP } from '@/utils/fetchDataFromWordPress';
-import Loading from '@/components/common/loading/Loading';
 import { BlogCardSkeleton } from '@/components/common/skeleton/Skeleton';
 
 export default function SectionBlogs() {
@@ -34,30 +32,27 @@ export default function SectionBlogs() {
         );
         const mappedPosts = await Promise.all(
           response.map(async (post: any) => {
-            // Fetch featured media (banner image) if available
             let bannerImg = '';
             if (post.featured_media) {
               const mediaResponse = await fetchDataFromWP(
                 `https://admin.syncible.io/wp-json/wp/v2/media/${post.featured_media}`
               );
-              bannerImg = mediaResponse.source_url || ''; // Ensure a default empty string if no image
+              bannerImg = mediaResponse.source_url || '';
             }
 
-            // Fetch author details
             const authorResponse = await fetchDataFromWP(
               `https://admin.syncible.io/wp-json/wp/v2/users/${post.author}`
             );
             const author = {
               name: authorResponse.name,
-              avatar_url: authorResponse.avatar_urls['96'], // Use the 96px avatar size
-              description: authorResponse.description, // Assuming a default position; you could add a custom field for this
+              avatar_url: authorResponse.avatar_urls['96'],
+              description: authorResponse.description,
             };
 
-            // Map to your ArticleEntry structure
             return {
               title: post.title.rendered,
               link: `/blogs/${post.slug}`,
-              bannerImg: bannerImg || '/SyncibleSmallerBanner.png', // Use a default image if no banner
+              bannerImg: bannerImg || '/SyncibleSmallerBanner.png',
               author: author,
             };
           })
@@ -74,7 +69,6 @@ export default function SectionBlogs() {
 
   return (
     <div className="w-full max-w-[90rem]" id="blogs">
-    {/* <div className="w-full" id="blogs"> */}
       <div className="flex flex-col items-center gap-8 px-4 py-[4.5rem] md:px-8 xl:px-32 xl:py-32 ">
         <div className="flex w-full items-center justify-between">
           <div className="text-[2rem] font-bold">{t('header')}</div>
